02484018a1eb2cedca276c30dac8237d7df8a256
Deepika <121660560+Deepika2104@users.noreply.github.com>
2024-07-09 15:08:28 +0530

    Add files via upload

---
+++ q2_max_card_score.py
@@ -0,0 +1,16 @@
+def max_score(points,k):
+    n=len(points)
+    total_sum=sum(points)
+    window_sum=sum(points[:n-k])
+    max_score=total_sum-window_sum
+    for i in range(n-k,n):
+        window_sum+=points[i]-points[i-(n-k)]
+        max_score = max(max_score,total_sum-window_sum)
+    return max_score
+
+# Example Test cases
+print(max_score([1,2,3,4,5,6,1], 3))    # output: 12
+print(max_score([2,2,2], 2))    # output: 4
+print(max_score([9,7,7,9,7,7,9], 7))    # output: 55
+# Custom Test cases
+print(max_score([1,2,6,3,1],3))     # output: 10
